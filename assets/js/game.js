b8942798f23a8b6ef47452e4b31488b182a743a2
Kim Mulligan <kimmariemulligan@gmail.com>
2020-08-22 21:34:51 -0700

    alerts and prompts not showing up

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,6 +9,7 @@ var enemyAttack = 12;
 var fight = function() {
   //Alert players that they are starting the rounc.
   window.alert("Welcome to Robot Gladiatiors");
+fight (
   //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable.
   enemyHealth = enemyHealth - playerAttack;
   console.log(
@@ -19,9 +20,18 @@ var fight = function() {
   //Subtract the value of 'enemyAtack' from the value of 'playerhealth' and use that result to update the value in the 'playerHealth' variable.
   playerHealth = playerHealth - enemyAttack;
   //Log a resulting message to the console so we know that it worked.
-  enemyName + "attacked" + playerName + "." + playerName + "now has " + playerHealth + "health remaining."
+  enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
 };
-
-fight();
+console.log(
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+  //check my enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died! ");
+  }
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+)
+);
 
  
