8c1bfd4c2bfa74160465137e4e8da5003359f8d1
Kim Mulligan <kimmariemulligan@gmail.com>
2020-08-22 21:58:55 -0700

    Corrections with Learning Assistant

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,32 +6,28 @@ var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function () {
   //Alert players that they are starting the rounc.
   window.alert("Welcome to Robot Gladiatiors");
-fight (
-  //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable.
-  enemyHealth = enemyHealth - playerAttack;
+    //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable.
+    enemyHealth = enemyHealth - playerAttack;
   console.log(
     playerName + "attacked " + enemyName + "now has " + enemyHealth + "enemy remaining "
-  
+
   );
   //Log a resulting message to the console so we know that it worked.
   //Subtract the value of 'enemyAtack' from the value of 'playerhealth' and use that result to update the value in the 'playerHealth' variable.
   playerHealth = playerHealth - enemyAttack;
   //Log a resulting message to the console so we know that it worked.
-  enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+  console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.")
 };
-console.log(
-  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.")
   //check my enemy's health
   if (enemyHealth <= 0) {
-    window.alert(enemyName + " has died! ");
-  }
-  else {
-    window.alert(enemyName + " still has " + enemyHealth + " health left.");
-  }
-)
-);
+  window.alert(enemyName + " has died! ");
+}
+else {
+  window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
 
- 
+
